12a6cf28e1b63afc746584ab04432a29e4e80ff6
Rob Gietema <rob@fourdigits.nl>
2009-09-29 17:33:26 +0000

    Added testing boilerplate.
    svn path=/sandbox/plone.app.deco/trunk/; revision=29995

--- plone/app/deco/tests.py
+++ plone/app/deco/tests.py
@@ -1,54 +1,35 @@
+"""Test setup for integration and functional tests."""
 import unittest
-
-from zope.testing import doctestunit
-from zope.component import testing
-from Testing import ZopeTestCase as ztc
 
 from Products.Five import zcml
 from Products.Five import fiveconfigure
+
+from Testing import ZopeTestCase as ztc
+
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
+from Products.PloneTestCase.layer import onsetup
 
 import plone.app.deco
 
-class TestCase(ptc.PloneTestCase):
-    class layer(PloneSite):
-        @classmethod
-        def setUp(cls):
-            fiveconfigure.debug_mode = True
-            zcml.load_config('configure.zcml',
-                             plone.app.deco)
-            fiveconfigure.debug_mode = False
+@onsetup
+def setup_product():
+    """Set up the package and its dependencies."""
 
-        @classmethod
-        def tearDown(cls):
-            pass
+    zcml.load_config('configure.zcml', plone.app.deco)
+    
+setup_product()
+ptc.setupPloneSite(extension_profiles=['plone.app.deco:default'])
 
+functional_tests = (
+    'browser.txt',
+    'controlpanel.txt',
+    )
 
 def test_suite():
-    return unittest.TestSuite([
-
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='plone.app.deco',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='plone.app.deco.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='plone.app.deco',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='plone.app.deco',
-        #    test_class=TestCase),
-
-        ])
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
+    """This sets up a test suite that actually runs the tests"""
+    return unittest.TestSuite(
+        [ztc.FunctionalDocFileSuite(
+            'tests/%s' % f, package='plone.app.deco',
+            test_class=ptc.FunctionalTestCase)
+            for f in functional_tests]
+        )
