5aebea9bf51c600d5be0189c2c0e9d84ec214ca5
Rob Gietema <rob@fourdigits.nl>
2009-11-02 00:19:18 +0000

    Added tests for decoDialog.
    svn path=/sandbox/plone.app.deco/trunk/; revision=31061

--- plone/app/deco/tests/javascripts/dialog.js
+++ plone/app/deco/tests/javascripts/dialog.js
@@ -10,3 +10,23 @@ test("Initialisation", function() {
 
     ok($.deco.dialog, "$.deco.dialog");
 });
+
+test("decoDialog", function() {
+    // expect(1);
+
+    // We'll create a div element for the dialog
+    var dialog = $(document.createElement("div"))
+        .append($(document.createElement("div"))
+            .addClass("formFields")
+            .append($(document.createElement("input")))
+        );
+    $(document.body).append(dialog);
+
+    // Init dialog
+    dialog.decoDialog();
+    dialog.find(".button-field").trigger("click");
+
+    equals($(".deco-dialog").length, 1, "Dialog added");
+    equals($(".deco-dialog-blocker").length, 1, "Dialog blocker added");
+    equals(dialog.find("input:visible").length, 0, "Inputs are hidden");
+});
