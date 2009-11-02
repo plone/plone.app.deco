76e5c93b012a70a060dc841f88181d5042519e52
Rob Gietema <rob@fourdigits.nl>
2009-11-02 23:21:53 +0000

    Added tests for dialog.
    svn path=/sandbox/plone.app.deco/trunk/; revision=31065

--- plone/app/deco/tests/javascripts/dialog.js
+++ plone/app/deco/tests/javascripts/dialog.js
@@ -1,7 +1,48 @@
 module("dialog", {
     setup: function () {
+        // We'll create a div element for the dialog
+        var dialog = $(document.createElement("div"))
+            .append($(document.createElement("form"))
+                .append($(document.createElement("ul"))
+                    .addClass("formTabs")
+                    .append($(document.createElement("li"))
+                        .addClass("formTab firstFormTab")
+                        .append($(document.createElement("a"))
+                            .addClass("selected")
+                        )
+                    )
+                    .append($(document.createElement("li"))
+                        .addClass("formTab")
+                        .append($(document.createElement("a")))
+                    )
+                    .append($(document.createElement("li"))
+                        .addClass("formTab lastFormTab")
+                        .append($(document.createElement("a")))
+                    )
+                )
+                .append($(document.createElement("fieldset"))
+                    .attr("id", "fieldset-default")
+                    .append($(document.createElement("div"))
+                        .addClass("row")
+                        .append($(document.createElement("div"))
+                            .attr("id", "title-field")
+                        )
+                    )
+                )
+                .append($(document.createElement("div"))
+                    .addClass("formFields")
+                    .append($(document.createElement("input")))
+                )
+            )
+            .attr("id", "region-content");
+        $(document.body).append(dialog);
+        $(document.body)
+            .append($(document.createElement("div"))
+                .addClass("deco-title-tile")
+            );
     },
     teardown: function () {
+        $("#region-content").remove();
     }
 });
 
@@ -12,21 +53,71 @@ test("Initialisation", function() {
 });
 
 test("decoDialog", function() {
-    // expect(1);
-
-    // We'll create a div element for the dialog
-    var dialog = $(document.createElement("div"))
-        .append($(document.createElement("div"))
-            .addClass("formFields")
-            .append($(document.createElement("input")))
-        );
-    $(document.body).append(dialog);
+    expect(3);
 
     // Init dialog
+    var dialog = $("#region-content");
     dialog.decoDialog();
     dialog.find(".button-field").trigger("click");
 
     equals($(".deco-dialog").length, 1, "Dialog added");
     equals($(".deco-dialog-blocker").length, 1, "Dialog blocker added");
     equals(dialog.find("input:visible").length, 0, "Inputs are hidden");
+});
+
+test("dialog.open", function() {
+    expect(0);
+    var optionsBackup = typeof($.deco.options) == "undefined" ? {} : $.deco.options;
+    $.deco.options = {
+        panels: $(document.body),
+        tiles: [{
+            'name': 'fields',
+            'label': 'Fields',
+            'tiles': [
+                {
+                    'name': 'title',
+                    'type': 'field',
+                    'id': 'title-field'
+                },
+                {
+                    'name': 'pony',
+                    'type': 'app'
+                },
+                {
+                    'name': 'text',
+                    'type': 'structure'
+                },
+            ]
+        }]
+    };
+
+    $("#region-content").decoDialog();
+    $.deco.dialog.open("all", {});
+
+    // TODO
+
+    $.deco.dialog.close();
+    equals($(".deco-dialog-blocker:visible").length, 0, "Dialog blocker removed");
+    equals($("#region-content:visible").length, 0, "Dialog removed");
+
+    $.deco.dialog.open("field", {});
+
+    // TODO
+
+    $.deco.dialog.close();
+    equals($(".deco-dialog-blocker:visible").length, 0, "Dialog blocker removed");
+    equals($("#region-content:visible").length, 0, "Dialog removed");
+
+    $.deco.options = optionsBackup;
+});
+
+test("dialog.openIframe", function() {
+    expect(3);
+
+    $.deco.dialog.openIframe("about:blank");
+    equals($(".deco-iframe-dialog").length, 1, "Iframe dialog added");
+
+    $.deco.dialog.close();
+    equals($(".deco-dialog-blocker:visible").length, 0, "Dialog blocker removed");
+    equals($(".deco-iframe-dialog").length, 0, "Iframe dialog removed");
 });
