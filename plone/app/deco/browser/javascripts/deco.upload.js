581bfc40d5f2aa8779bf1fa64d435713edf8aa03
Rob Gietema <rob@fourdigits.nl>
2009-11-06 18:51:37 +0000

    Added  upload module.
    svn path=/sandbox/plone.app.deco/trunk/; revision=31161

---
+++ plone/app/deco/browser/javascripts/deco.upload.js
@@ -0,0 +1,14 @@
+/**
+ * This plugin is used to upload files and images.
+ *
+ * @author Rob Gietema
+ * @version 0.1
+ */
+;(function($) {
+
+    // Define deco namespace if it doesn't exist
+    if (typeof($.deco) == "undefined") {
+        $.deco = {};
+    }
+
+})(jQuery);
