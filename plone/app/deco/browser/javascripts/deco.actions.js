27720b9123fdc1774cb7124e0dada1cc58f20cdb
Rob Gietema <rob@fourdigits.nl>
2010-05-05 15:06:16 +0000

    Removed blur action.
    svn path=/sandbox/plone.app.deco/trunk/; revision=36504

--- plone/app/deco/browser/javascripts/deco.actions.js
+++ plone/app/deco/browser/javascripts/deco.actions.js
@@ -249,39 +249,6 @@
             }
         });
 
-        // Register blur
-        $.deco.registerAction('blur', {
-            exec: function() {
-
-                // Remove left and right align classes
-                var img = $(".deco-selected-tile").children(".deco-tile-content").children("img").get(0);
-
-                // Create an empty canvas element
-                var canvas = document.createElement("canvas");
-                canvas.width = img.width;
-                canvas.height = img.height;
-
-                // Copy the image contents to the canvas
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(img, 0, 0);
-
-                ctx.fillStyle = "red";
-
-                ctx.beginPath();
-                ctx.moveTo(30, 30);
-                ctx.lineTo(150, 150);
-                ctx.quadraticCurveTo(60, 70, 70, 150);
-                ctx.lineTo(30, 30);
-                ctx.fill();
-
-                // Get the data-URL formatted image
-                // Firefox supports PNG and JPEG. You could check img.src to guess the
-                // original format, but be aware the using "image/jpg" will re-encode the image.
-                var dataURL = canvas.toDataURL("image/png");
-                $(img).attr("src", dataURL);
-            }
-        });
-
         // Register justify left action
         $.deco.registerAction('justify-left', {
             exec: function() {
