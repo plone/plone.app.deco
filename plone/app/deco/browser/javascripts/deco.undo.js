162a48ae3b617216627db7c95d872e5d56b5a04b
Duco Dokter <dokter@w20e.com>
2010-08-25 16:29:44 +0000

    Made JSLint compatible
    svn path=/sandbox/plone.app.deco/trunk/; revision=38961

--- plone/app/deco/browser/javascripts/deco.undo.js
+++ plone/app/deco/browser/javascripts/deco.undo.js
@@ -30,7 +30,7 @@ immed: true, strict: true, maxlen: 80, maxerr: 9999 */
     }
 
     // Declare deco.undo namespace
-    $.deco.undo = function() {};
+    $.deco.undo = function () {};
 
     /**
      * Stack constructor, taking optional size parameter.
@@ -38,36 +38,36 @@ immed: true, strict: true, maxlen: 80, maxerr: 9999 */
      * @param {Integer} stackSize Maximum number of items on the stack.
      */
     $.deco.undo.Stack = function (stackSize) {
-      if (typeof(stackSize) === "undefined") {
-        this.maxsize = 10;
-      } else {
-        this.maxsize = stackSize;
-      }
+        if (typeof(stackSize) === "undefined") {
+            this.maxsize = 10;
+        } else {
+            this.maxsize = stackSize;
+        }
 
-      this.stack = Array();
+        this.stack = [];
     };
 
     /**
      * Return current stack size.
      * @id jQuery.deco.undo.Stack.size
      */
-    $.deco.undo.Stack.prototype.size  = function() {
+    $.deco.undo.Stack.prototype.size  = function () {
         return this.stack.length;
     };
 
     /**
-     * FIFO stack push, that removes object at other end if the stack grows bigger than
-     * the size set.
+     * FIFO stack push, that removes object at other end if the stack
+     * grows bigger than the size set.
      * @id jQuery.deco.undo.Stack.add
      * @param {Object} obj Object to push onto the stack.
      */
-    $.deco.undo.Stack.prototype.add = function(obj) {
+    $.deco.undo.Stack.prototype.add = function (obj) {
 
-      if (this.stack.length >= this.maxsize) {
-        this.stack.pop();
-      }
+        if (this.stack.length >= this.maxsize) {
+            this.stack.pop();
+        }
 
-      this.stack.unshift(obj);
+        this.stack.unshift(obj);
     };
 
     /**
@@ -75,24 +75,24 @@ immed: true, strict: true, maxlen: 80, maxerr: 9999 */
      * through jQuery.deco.undo.Stack.add) are added (using shift) at index 0.
      * @id jQuery.deco.undo.Stack.get
      */
-    $.deco.undo.Stack.prototype.get = function(i) {
-      return this.stack[i];
+    $.deco.undo.Stack.prototype.get = function (i) {
+        return this.stack[i];
     };
 
     /**
-     * Undo manager, handling calls to undo/redo. This implementation uses full DOM
-     * snippets.
+     * Undo manager, handling calls to undo/redo. This implementation
+     * uses full DOM snippets.
      * @id jQuery.deco.undo.UndoManager
      * @param {Integer} stackSize max undo history
      * @param {Function} handler for undo/redo, taking state as argument
      * @param {Object} currentState Current state
      */
-    $.deco.undo.UndoManager = function(stackSize, handler, currentState) {
+    $.deco.undo.UndoManager = function (stackSize, handler, currentState) {
       
-      this.stack = new $.deco.undo.Stack(stackSize);
-      this.pointer = 0;
-      this.handler = handler;
-      this.stack.add(currentState);
+        this.stack = new $.deco.undo.Stack(stackSize);
+        this.pointer = 0;
+        this.handler = handler;
+        this.stack.add(currentState);
     };
 
     /**
@@ -100,41 +100,41 @@ immed: true, strict: true, maxlen: 80, maxerr: 9999 */
      * @id jQuery.deco.undo.UndoManager.add
      * @param {Object} state State to add.
      */
-    $.deco.undo.UndoManager.prototype.add = function(state) {
+    $.deco.undo.UndoManager.prototype.add = function (state) {
       
-      this.stack.add(state);
-    }
+        this.stack.add(state);
+    };
 
     /**
      * Undo last action, by restoring last state.
      * @id jQuery.deco.undo.UndoManager.undo
      */
-    $.deco.undo.UndoManager.prototype.undo = function() {
+    $.deco.undo.UndoManager.prototype.undo = function  () {
       
-      var state = this.stack.get(this.pointer + 1);
+        var state = this.stack.get(this.pointer + 1);
 
-      if (state) {
-        this.handler(state);
-        this.pointer++;
-      } else {
-        // Alert there's no (more) states.
-      }
+        if (state) {
+            this.handler(state);
+            this.pointer += 1;
+        } else {
+          // Alert there's no (more) states.
+        }
     };
 
     /**
      * Redo last action, by calling handler with previous state.
      * @id jQuery.deco.undo.UndoManager.redo
      */
-    $.deco.undo.UndoManager.prototype.redo = function() {
+    $.deco.undo.UndoManager.prototype.redo = function () {
 
-      var state = this.stack.get(this.pointer - 1);
+        var state = this.stack.get(this.pointer - 1);
 
-      if (state) {
-        this.handler(state);
-        this.pointer--;
-      } else {
-        // Alert there's no (more) states.
-      }
+        if (state) {
+            this.handler(state);
+            this.pointer -= 1;
+        } else {
+            // Alert there's no (more) states.
+        }
     };
 
 }(jQuery));
