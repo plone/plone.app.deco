daaa841cda0518e51c68008cbf28ab6f687c4603
Martijn Jacobs <martijn@fourdigits.nl>
2010-08-26 12:43:50 +0000

    Added tests from trunk
    svn path=/sandbox/plone.app.deco/branches/10910-fixregistry/; revision=39004

---
+++ plone/app/deco/tests/test_browser.py
@@ -0,0 +1,26 @@
+import unittest2 as unittest
+import doctest
+import pprint
+import interlude
+from plone.testing import layered
+
+from plone.app.deco.tests.base import PADECO_FUNCTIONAL_TESTING
+
+optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+testfiles = (
+    'test_browser_decoconfig.txt',
+    'test_browser_controlpanel.txt',
+)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(doctest.DocFileSuite(test,
+                                     optionflags=optionflags,
+                                     globs={'interact': interlude.interact,
+                                            'pprint': pprint.pprint,
+                                            }),
+                layer=PADECO_FUNCTIONAL_TESTING)
+        for test in testfiles])
+    return suite
