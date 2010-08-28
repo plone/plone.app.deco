fdbfdcac46f4430ffff4e0b04eb99d175aac3e1d
Simone Deponti <shywolf9982@gmail.com>
2010-08-28 12:33:20 +0000

    Fixed plone.autoform _getDisallowedFields
    svn path=/sandbox/plone.app.deco/trunk/; revision=39222

--- plone/app/deco/utils.py
+++ plone/app/deco/utils.py
@@ -1,6 +1,9 @@
 from zope.component import queryUtility, getMultiAdapter
 from zope.interface import Interface
 
+from zope.security.interfaces import IPermission
+from AccessControl import getSecurityManager
+
 from z3c.form.interfaces import IEditForm, IFieldWidget, DISPLAY_MODE, \
                                 HIDDEN_MODE
 from plone.supermodel.utils import mergedTaggedValueDict, mergedTaggedValueList
@@ -8,9 +11,32 @@ from plone.autoform.interfaces import IFormFieldProvider
 from plone.autoform.interfaces import OMITTED_KEY, WIDGETS_KEY, MODES_KEY
 from plone.autoform.interfaces import READ_PERMISSIONS_KEY, \
                                       WRITE_PERMISSIONS_KEY
-from plone.autoform.utils import _getDisallowedFields
+
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import resolveDottedName
+
+
+# BBB: this should be in plone.autoform.utils
+class PermissionChecker(object):
+
+    def __init__(self, permissions, context):
+        self.permissions = permissions
+        self.context = context
+        self.sm = getSecurityManager()
+        self.cache = {}
+
+    def allowed(self, field_name):
+        permission_name = self.permissions.get(field_name, None)
+        if permission_name is not None:
+            if permission_name not in self.cache:
+                permission = queryUtility(IPermission, name = permission_name)
+                if permission is None:
+                    self.cache[permission_name] = True
+                else:
+                    self.cache[permission_name] = bool(
+                        self.sm.checkPermission(permission.title, self.context)
+                    )
+        return self.cache.get(permission_name, True)
 
 
 def mergedTaggedValuesForIRO(schema, name, iro):
@@ -79,18 +105,29 @@ def extractFieldInformation(schema, context, request, prefix):
     omitted = mergedTaggedValuesForIRO(schema, OMITTED_KEY, iro)
     modes = mergedTaggedValuesForIRO(schema, MODES_KEY, iro)
     widgets = mergedTaggedValueDict(schema, WIDGETS_KEY)
+
     if context is not None:
-        tagged_values = mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY)
-        for field in _getDisallowedFields(context, tagged_values, ''):
-            omitted[field] = 'true'
+        read_permissionchecker = PermissionChecker(
+            mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY),
+            context
+        )
+        write_permissionchecker = PermissionChecker(
+            mergedTaggedValueDict(schema, WRITE_PERMISSIONS_KEY),
+            context
+        )
     tagged_values = mergedTaggedValueDict(schema, WRITE_PERMISSIONS_KEY)
-    read_only = _getDisallowedFields(context, tagged_values, '')
+    read_only = []
     for name, mode in modes.items():
         if mode == HIDDEN_MODE:
             omitted[name] = True
         elif mode == DISPLAY_MODE:
             read_only.append(name)
     for name in schema.names(True):
+        if context is not None:
+            if not read_permissionchecker.allowed(name):
+                omitted[name] = True
+            if not write_permissionchecker.allowed(name):
+                read_only.append(name)
         if is_visible(name, omitted):
             yield {
                 'name': prefix + name,
