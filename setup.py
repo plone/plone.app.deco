aeaf9c5efa8b61355c7ac1d53a5404b75af78aec
Rob Gietema <rob@fourdigits.nl>
2010-09-16 13:06:09 +0000

    Removed windmill.
    svn path=/plone.app.deco/trunk/; revision=39934

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(name='plone.app.deco',
           'test': [
             'interlude',
             'unittest2',
-            'niteoweb.windmill',
             'plone.app.layoutbehavior',
             'plone.app.page',
             'plone.app.testing', ]},
