4b677274e25821bf2d0c9d71c2b5ab297d755380
Rob Gietema <rob@fourdigits.nl>
2011-09-21 13:27:34 +0200

    add test stubs for deco editor module

---
+++ plone/app/deco/tests/javascripts/editor.js
@@ -0,0 +1,25 @@
+module("editor", {
+});
+
+test("Apply block formatting", function() {
+});
+
+test("Apply inline formatting", function() {
+});
+
+test("Apply block formatting with a classname", function() {
+});
+
+test("Apply inline formatting with a classname", function() {
+});
+
+test("Apply block formatting to a selection covering multiple block elements", function() {
+});
+
+test("Apply inline formatting to a selection covering multiple block elements", function() {
+});
+
+test("Apply inline formatting to a selection spanning partial elements", function() {
+    // <p><b>some text</b> here</p>
+});
+
