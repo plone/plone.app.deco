7ddb090631fc7dc6d8083a641af4d50138748769
Ross Patterson <me@rpatterson.net>
2011-09-23 15:09:10 -0700

    Use the deco test layer for selenium tests

--- plone/app/deco/tests/selenium/test_selenium.py
+++ plone/app/deco/tests/selenium/test_selenium.py
@@ -2,14 +2,21 @@ import unittest2 as unittest
 
 from plone.app.testing import selenium_layers as layers
 
+from plone.app.deco import testing
+
+DECO_SELENIUM_TESTING = testing.FunctionalTesting(
+    bases=(testing.DECO_FUNCTIONAL_TESTING,
+           layers.SELENIUM_PLONE_FUNCTIONAL_TESTING),
+    name="Deco:Selenium")
+
 
 class SimpleScenarioTestCase(unittest.TestCase):
 
-    layer = layers.SELENIUM_PLONE_FUNCTIONAL_TESTING
+    layer = DECO_SELENIUM_TESTING
 
     def test_basic_deco(self):
 
         portal = self.layer['portal']
-        sel = self.layer['selenium']
+        selenium = self.layer['selenium']
 
-        layers.open(sel, portal.absolute_url())
+        layers.login(selenium, portal)
