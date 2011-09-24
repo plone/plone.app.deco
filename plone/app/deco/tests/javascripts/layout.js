310f8d1d01a034cc63b8ccce30913cc5f93eeeb6
Tom Lazar <tom@tomster.org>
2011-09-24 17:10:16 +0200

    ensure that text tiles are saved

--- plone/app/deco/tests/javascripts/layout.js
+++ plone/app/deco/tests/javascripts/layout.js
@@ -51,6 +51,21 @@ $.deco.options = {
           "weight" : 20,
           "widget" : "z3c.form.browser.text.TextWidget",
           "id" : "formfield-form-widgets-alcohol_volume"
+        },
+        { "available_actions" : [ "tile-align-block",
+              "tile-align-right",
+              "tile-align-left"
+            ],
+          "category" : "structure",
+          "default_value" : null,
+          "favorite" : false,
+          "label" : "Text",
+          "name" : "text",
+          "read_only" : false,
+          "rich_text" : true,
+          "settings" : true,
+          "tile_type" : "text",
+          "weight" : 20
         }
       ],
     "weight" : 30
@@ -72,7 +87,7 @@ module("layout", {
     setup: function () {
         // We'll create a div element for the overlay
         $(document.body)
-            .append($('<div data-panel="content"><div class="deco-grid-row"><div class="deco-grid-cell"><div class="deco-tile deco-alcohol_volume-tile"><div class="deco-tile-content"><span data-tile="./@@plone.app.standardtiles.field?field=alcohol_volume">5.4</span></div></div></div></div></div><div data-panel="portal-column-one"><div class="deco-grid-row"><div class="deco-grid-cell"><div class="deco-tile deco-plone.app.standardtiles.title-tile"><div class="deco-tile-content"><span data-tile="./@@plone.app.standardtiles.title">Samuel L. Ipsum</span></div></div></div></div></div>'));
+            .append($('<div data-panel="content"><div class="deco-grid-row"><div class="deco-grid-cell"><div class="deco-tile deco-alcohol_volume-tile"><div class="deco-tile-content"><span data-tile="./@@plone.app.standardtiles.field?field=alcohol_volume">5.4</span></div></div><div class="deco-tile deco-text-tile"><div class="deco-tile-content"><p>Free <strong>text</strong></p></div></div></div></div></div><div data-panel="portal-column-one"><div class="deco-grid-row"><div class="deco-grid-cell"><div class="deco-tile deco-plone.app.standardtiles.title-tile"><div class="deco-tile-content"><span data-tile="./@@plone.app.standardtiles.title">Samuel L. Ipsum</span></div></div></div></div></div>'));
         $(document.body)
             .append(
                 $(document.createElement("div"))
@@ -105,7 +120,7 @@ test("Initialisation", function() {
 });
 
 test("Init without data", function() {
-    expect(6);
+    expect(7);
 
     // Init panel
     $.deco.options.panels.decoLayout();
@@ -118,5 +133,5 @@ test("Init without data", function() {
     equals($("#formfield-form-widgets-IDublinCore-title input").val(), "Samuel L. Ipsum", "title value preserved in form");
     equals(saved_html.indexOf('<span data-tile="./@@plone.app.standardtiles.field?field=alcohol_volume"></span>') != -1, true, "getPageContent preserves custom field");
     equals($("#formfield-form-widgets-alcohol_volume input").val(), "5.4", "custom field value preserved in form");
-
+    equals(saved_html.indexOf("<p>Free <strong>text</strong></p>") != -1, true, "text tiles are preserved");
 });
