2f9e1b81ee915c7afa4d77d7062b2dfc69ff170c
Rok Garbas <rok@garbas.si>
2011-11-07 11:23:49 -0800

    renaming README.txt to README.rst to please github

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tests_require = ['interlude',
 setup(name='plone.app.deco',
       version=version,
       description="Package for Deco UI functionality",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
