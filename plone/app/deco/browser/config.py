681d4cdcc8e73f01729ed369aa065e75245aa094
Rok Garbas <rok@garbas.si>
2011-12-11 20:24:54 +0100

    no need for extra request for configuration
     - added plone.deco.config viewlet and places it in plone.top viewlet manager
     - above manager sets $.deco.default_config
     - creating @@deco-config.json (not sure if we'll need it but i leave it)

---
+++ plone/app/deco/browser/config.py
@@ -0,0 +1,60 @@
+
+from zope.publisher.browser import BrowserView
+from zope.component import getUtility
+try:
+    import json
+except:
+    import simplejson as json
+
+from plone.registry.interfaces import IRegistry
+from plone.app.deco.interfaces import IDecoRegistry
+from Products.CMFCore.utils import getToolByName
+
+
+class DecoConfigView(BrowserView):
+
+    def obtain_type(self):
+        """ Obtains the type of the context object or of the object we are
+            adding.
+        """
+
+        if 'type' in self.request.form:
+            return self.request.form['type']
+
+        else:
+            if hasattr(self.context, 'portal_type'):
+                return self.context.portal_type
+
+        return None
+
+    @property
+    def config(self):
+        registry = IDecoRegistry(getUtility(IRegistry))
+        config = registry(**{
+            'type': self.obtain_type(),
+            'context': self.context,
+            'request': self.request,
+            })
+
+        # does user have permission to change layout
+        membership = getToolByName(self.context, 'portal_membership')
+        can_change_layout = membership.checkPermission(
+                'Plone: Change Deco Layout', self.context)
+
+        # remove add-tile and insert action from config if user doesn't have
+        # permission to change layout 
+        config['can_change_layout'] = bool(can_change_layout)
+        if not can_change_layout:
+            if 'insert' in config['default_available_actions']:
+                config['default_available_actions'].remove('insert')
+            if 'add-tile' in config['default_available_actions']:
+                config['default_available_actions'].remove('add-tile')
+
+        return json.dumps(config)
+
+
+class DecoConfigJSONView(DecoConfigView):
+
+    def __call__(self):
+        self.request.response.setHeader('Content-Type', 'application/json')
+        return self.config
